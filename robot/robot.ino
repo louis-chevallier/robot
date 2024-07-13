e89a52b97f742548e028629d3c2324151255883b
jitdeep chehara <jitdeep.chehara@gmail.com>
2024-07-13 15:25:25 +0200

    v0

--- robot/robot.ino
+++ robot/robot.ino
@@ -1,3 +1,11 @@
+//https://randomnerdtutorials.com/esp8266-nodemcu-dc-motor-l298n-motor-driver-control-speed-direction/
+
+
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp8266-nodemcu-dc-motor-l298n-motor-driver-control-speed-direction/
+*********/
+
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <DallasTemperature.h>
@@ -8,8 +16,7 @@
 #include <Arduino.h>
 #include <microTuple.h>
 
-#include <WebSocketsServer.h>
-
+// server http => http://192.168.1.177/
 
 String S;
 
@@ -51,8 +58,15 @@ String page(R""""(
   </html>
 )"""");
 
-
-
+void handle_speed() {
+  String v = server.arg("value");
+  auto vi = v.toInt();
+  String r = String("set speed to :") + vi;
+  EKOX(r);
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  server.send(200, "text/html", r.c_str());
+  Serial.println("end");
+}
 
 void handle_index() {
   Serial.println("index");
@@ -62,15 +76,6 @@ void handle_index() {
   server.send(200, "text/html", npage.c_str());
   Serial.println("end");
 }
-
-void handle_x() {
-  // method get
-  String npage("ok");
-  server.sendHeader("Access-Control-Allow-Origin", "*");
-  server.send(200, "text/html", npage.c_str());
-  Serial.println("end");
-};
-
 
 void webSetup() {
   
@@ -91,38 +96,101 @@ void webSetup() {
   // Print the IP address
   Serial.println(WiFi.localIP());
   server.on("/", handle_index); //Handle Index page
-  server.on("/x", handle_x);
+  server.on("/speed", handle_speed); //Handle Index page
   
   server.begin(); //Start the server
   Serial.println("setup");
-  pinMode(2, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
-  digitalWrite(2, HIGH);  
   Serial.println("Server listening");
 }
+
 
 void webLoop() {
   server.handleClient(); //Handling of incoming client requests
 }
 
-/***********************/
+
+// Motor A
+int motor1Pin1 = 12; 
+int motor1Pin2 = 14; 
+int enable1Pin = 13; 
+
+// Setting minimum duty cycle
+int dutyCycle = 60;
 
 void setup() {
-  // put your setup code here, to run once:
+
+
+// put your setup code here, to run once:
   Serial.begin(115200); //Begin Serial at 115200 Baud
   Serial.print("starting");
   EKO();
   delay(10);
   webSetup();
   
-  EKOX("ok");
+  EKOT("ok");
+  /*
   EKOX(D1);
   EKOX(D2);
   EKOX(D3);
   EKOX(D4);
   EKOX(D5);
+*/
+
+  //Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
   webLoop();
+
+}
+
+void motor() {
+  // sets the pins as outputs:
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+  pinMode(enable1Pin, OUTPUT);
+
+
+  // testing
+  EKOT("Testing DC Motor...");
+
+  //Apply power to spin at maximum speed
+  digitalWrite(enable1Pin, HIGH);
+
+  // Move the DC motor forward at maximum speed
+  Serial.println("Moving Forward");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH); 
+  delay(2000);
+
+  // Stop the DC motor
+  Serial.println("Motor stopped");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW);
+  delay(1000);
+
+  // Move DC motor backwards at maximum speed
+  Serial.println("Moving Backwards");
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW); 
+  delay(2000);
+
+  // Stop the DC motor
+  Serial.println("Motor stopped");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW);
+  delay(1000);
+
+  // Move DC motor forward with increasing speed
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW);
+  while (dutyCycle <= 255){
+    analogWrite(enable1Pin, dutyCycle);   
+    Serial.print("Forward with duty cycle: ");
+    Serial.println(dutyCycle);
+    dutyCycle = dutyCycle + 5;
+    delay(500);
+  }
+  dutyCycle = 60;
 }
